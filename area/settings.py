b131cc1e29afc60b6b9df32ce4ebb8187d1d7ebe
ReginaWangui <reginahwangui801@gmail.com>
2021-04-13 20:30:17 +0300

    deployment

--- area/settings.py
+++ area/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='nz2uu9bp6qpw7d@l0%$g1fcuwo_&^7iv97x4+h@s#m#10pe=qz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
